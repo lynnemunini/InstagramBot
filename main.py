c59eb6895d4392f33a284b71bf96cab7bc8d3405
Lynne Munini <muninilynne65@gmail.com>
2021-11-24 23:12:21 -0500

    Follows back

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from homepage import HomePage
 import os
-# from instapy import InstaPy
 s = Service("/home/lynne/Programs/Development/chromedriver")
 driver = webdriver.Chrome(service=s)
 driver.implicitly_wait(5)
@@ -10,6 +9,5 @@ driver.implicitly_wait(5)
 home_page = HomePage(driver)
 login_page = home_page.go_to_login_page()
 login_page.login(os.environ["USER"], os.environ["PASSWORD"])
-# session = InstaPy(username=os.environ["USER"], password=os.environ["PASSWORD"])
 
 # driver.close()
