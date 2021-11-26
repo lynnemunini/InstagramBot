fa22586a2657fd9ab336a70913e8bd358fa06271
Lynne Munini <muninilynne65@gmail.com>
2021-11-26 15:08:44 -0500

    Handles exception

--- login.py
+++ login.py
@@ -1,7 +1,7 @@
 import time
 import os
 from selenium.webdriver.common.by import By
-
+from selenium.common.exceptions import NoSuchElementException
 
 class LoginPage:
     def __init__(self, driver):
@@ -27,19 +27,25 @@ class LoginPage:
     def follow(self):
         activity = self.driver.find_element(By.CSS_SELECTOR, "#react-root > section > nav > div._8MQSO.Cx7Bp > div > div > div.ctQZg.KtFt3 > div > div:nth-child(5) > a > svg")
         activity.click()
+        # time.sleep(5)
+        # requests = self.driver.find_element(By.CSS_SELECTOR, "#react-root > section > nav > div._8MQSO.Cx7Bp > div > div > div.ctQZg.KtFt3 > div > div:nth-child(5) > div > div.uo5MA._2ciX.tWgj8.XWrBI > div._01UL2 > div > div > div > div.PUHRj.eKc9b.H_sJK > div.iTMfC > div > span > svg")
+        # requests.click()
         time.sleep(5)
-        requests = self.driver.find_element(By.CSS_SELECTOR, "#react-root > section > nav > div._8MQSO.Cx7Bp > div > div > div.ctQZg.KtFt3 > div > div:nth-child(5) > div > div.uo5MA._2ciX.tWgj8.XWrBI > div._01UL2 > div > div > div > div.PUHRj.eKc9b.H_sJK > div.iTMfC > div > span > svg")
-        requests.click()
-        time.sleep(5)
-        requests_follow = self.driver.find_elements(By.XPATH, "//button[.='Confirm']")
-        for request in requests_follow:
-            request.click()
-            time.sleep(2)
+        try:
+            requests_follow = self.driver.find_elements(By.XPATH, "//button[.='Confirm']")
+        except NoSuchElementException:
+            self.driver.close()
+        else:
+            for request in requests_follow:
+                request.click()
+                time.sleep(2)
 
-        # follows = self.driver.find_elements(By.XPATH, "//button[.='Follow']")
-        follows = self.driver.find_elements(By.CLASS_NAME, "iTMfC")
-        # print(len(follows))
-        for follow in follows:
-            follow.click()
-            time.sleep(2)
+            # follows = self.driver.find_elements(By.XPATH, "//button[.='Follow']")
+            follows = self.driver.find_elements(By.CLASS_NAME, "iTMfC")
+            # print(len(follows))
+            for follow in follows:
+                follow.click()
+                time.sleep(2)
+
+
 
